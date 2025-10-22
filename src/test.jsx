da5acc8360bcda30a7039d85a6689c1e06c05a9f
kvrathan2718@gmail.com <kvrathan2718@gmail.com>
2025-10-22 13:57:11 +0530

    Update dashboard: added App.jsx, removed old App.js, updated package.json and package-lock.json

---
+++ src/test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div>
+      <h1>Hello JSX Highlighting!</h1>
+      <p>This should be colored in VS Code.</p>
+    </div>
+  );
+}
+
+export default Test;
